3a73561763e558939f21d2d315e0510e3fb50dfc
juhwanz_w <ghdwnghks209@naver.com>
2026-06-23 12:30:22 +0900

    dw

--- src/git_java/Test.java
+++ src/git_java/Test.java
@@ -3,7 +3,7 @@ package git_java;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("TEST1");
-		System.out.println("TEST321412414");
+		System.out.println("T야하");
 		System.out.println("TEST322");
 
 	}
